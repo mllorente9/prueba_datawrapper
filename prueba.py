b77524249ad6659b54f98e6c6685673ab29db9b3
miguel.llorente <miguel.llorente@bosonit.com>
2023-05-09 08:43:09 +0200

    ok

--- prueba.py
+++ prueba.py
@@ -5,13 +5,13 @@ import subprocess
 hdfs_path = 'C:\\Users\\miguel.llorente\\Documents\\Nastat\\nastat\\hive\\distribution\\Industria_construccion_y_servicios\\040106_produccion_turismos.sh'
 
 # Ruta local donde deseas almacenar el archivo descargado
-local_path = 'C:\Users\miguel.llorente\Desktop'
+local_path = 'C:\\Users\\miguel.llorente\\Desktop\\'
 
 # Descarga el archivo de HDFS a tu sistema local
 subprocess.call(['hadoop', 'fs', '-copyToLocal', hdfs_path, local_path])
 
 # Agrega el archivo al repositorio Git y realiza un commit
-git_path = 'C:\Users\miguel.llorente\Documents\Nastat\prueba_datawrapper\prueba_datawrapper'
+git_path = 'C:\\Users\\miguel.llorente\\Documents\\Nastat\\prueba_datawrapper\\prueba_datawrapper'
 git_file = os.path.join(git_path, 'nombre_del_archivo')
 
 subprocess.call(['git', '-C', git_path, 'add', git_file])
